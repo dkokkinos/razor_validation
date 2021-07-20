87dc6162b81b2206ccc96ae260cf4821f8e316bf
dkokkinos <dmt.kokkinos@gmail.com>
2021-07-20 13:29:06 +0300

    add custom error message for ISBN code

--- ValidationApplication/Models/BookModel.cs
+++ ValidationApplication/Models/BookModel.cs
@@ -12,7 +12,8 @@ namespace ValidationApplication.Models
         public string SKU { get; set; }
 
         [Required]
-        [RegularExpression("^(?:ISBN(?:-13)?:?\\ )?(?=[0-9]{13}$|(?=(?:[0-9]+[-\\ ]){4})[-\\ 0-9]{17}$)97[89][-\\ ]?[0-9]{1,5}[-\\ ]?[0-9]+[-\\ ]?[0-9]+[-\\ ]?[0-9]$")]
+        [RegularExpression("^(?:ISBN(?:-13)?:?\\ )?(?=[0-9]{13}$|(?=(?:[0-9]+[-\\ ]){4})[-\\ 0-9]{17}$)97[89][-\\ ]?[0-9]{1,5}[-\\ ]?[0-9]+[-\\ ]?[0-9]+[-\\ ]?[0-9]$",
+            ErrorMessage = "{0} must be a valid ISBN code")]
         public string ISBN { get; set; }
 
         [Required]
