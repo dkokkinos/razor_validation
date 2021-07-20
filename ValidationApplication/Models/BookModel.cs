46cb157f61c2c9226890f955c1946073ca0ee476
dkokkinos <dmt.kokkinos@gmail.com>
2021-07-20 13:27:04 +0300

    add regex attribute for ISBN

--- ValidationApplication/Models/BookModel.cs
+++ ValidationApplication/Models/BookModel.cs
@@ -12,6 +12,7 @@ namespace ValidationApplication.Models
         public string SKU { get; set; }
 
         [Required]
+        [RegularExpression("^(?:ISBN(?:-13)?:?\\ )?(?=[0-9]{13}$|(?=(?:[0-9]+[-\\ ]){4})[-\\ 0-9]{17}$)97[89][-\\ ]?[0-9]{1,5}[-\\ ]?[0-9]+[-\\ ]?[0-9]+[-\\ ]?[0-9]$")]
         public string ISBN { get; set; }
 
         [Required]
